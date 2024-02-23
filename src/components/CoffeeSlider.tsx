b889790fedab2b0f8c5b42d34e840c95d42976a2
Alireza <alireza83safarii@gmail.com>
2024-02-23 16:14:24 +0330

    feat: create coffee slider

---
+++ src/components/CoffeeSlider.tsx
@@ -0,0 +1,77 @@
+"use client";
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import Coffee from "./Coffee";
+
+const CoffeeSlider = () => {
+  const breakpoints = {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 30,
+    },
+  };
+  return (
+    <div className="m-auto max-w-[1080px] my-20">
+      <div className="flex justify-center items-center">
+        <b className="flex-1 bg-gray-200 h-[2px]"></b>
+        <h1 className="text-xl px-4">قهوه های ترکیبی</h1>
+        <b className="flex-1 bg-gray-200 h-[2px]"></b>
+      </div>
+
+      <Swiper
+        slidesPerView={5}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        loop
+        className="mySwiper"
+        breakpoints={breakpoints}
+      >
+        <SwiperSlide>
+          <Coffee />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Coffee />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Coffee />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Coffee />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Coffee />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Coffee />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Coffee />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Coffee />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Coffee />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default CoffeeSlider;
