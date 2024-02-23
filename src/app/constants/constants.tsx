93be6a959176396a68fe46c0e4b27ee998db8c9d
Alireza <alireza83safarii@gmail.com>
2024-02-23 17:44:09 +0330

    feat: add productCategory contants

---
+++ src/app/constants/constants.tsx
@@ -0,0 +1,7 @@
+export const productCategory = [
+  { id: 1, titel: "تجهیزات خانگی", image: "/image/aa.jpg" },
+  { id: 2, titel: "تجهیزات کافی شاپ", image: "/image/coffeeEquipment.jpg" },
+  { id: 3, titel: "اسپرسوساز", image: "/image/espersosaz.png" },
+  { id: 4, titel: "قهوه", image: "/image/ghahve.webp" },
+  { id: 5, titel: "بار سرد", image: "/image/mohoto.png" },
+];
