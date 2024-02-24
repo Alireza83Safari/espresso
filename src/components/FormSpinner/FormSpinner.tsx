949e3019cc1285206e887cfea9d778b81abc5fc3
Alireza <alireza83safarii@gmail.com>
2024-02-24 10:48:52 +0330

    feat: add form spinner

---
+++ src/components/FormSpinner/FormSpinner.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./FormSpinner.css";
+
+const FormSpinner = () => {
+  return (
+    <div className="lds-ellipsis">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default FormSpinner;
