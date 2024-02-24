7551c9b92843379091411c2636cf317dbcc6e6a4
Alireza <alireza83safarii@gmail.com>
2024-02-24 11:16:04 +0330

    feat: add custom breakpoints

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,31 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "banner-1": "url('/image/banner.webp')",
+        "banner-2": "url('/image/banner-2.webp')",
+      },
+      colors: {
+        green: "#204D48",
+        textGray: "#777777",
+      },
+      screens: {
+        xs: "500px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
