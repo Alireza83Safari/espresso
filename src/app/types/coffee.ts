c711189150cae1753ae1a7f4d12d60c6f940cdc1
Alireza <alireza83safarii@gmail.com>
2024-02-25 21:27:48 +0330

    feat: add coffee type

---
+++ src/app/types/coffee.ts
@@ -0,0 +1,14 @@
+export interface CoffeeType {
+  _id: string;
+  name: string;
+  price: number;
+  seed: string;
+  seedType: string;
+  image: string;
+  label: string;
+  description: string;
+  caffeine: string;
+  weight: number;
+  createdAt: string;
+  updatedAt: string;
+}
