3eb3b447a832b8ab171c50de4561f1ea14760a7b
Alireza <alireza83safarii@gmail.com>
2024-02-25 21:25:58 +0330

    feat: add coffees page

---
+++ src/app/coffee/components/FilterCoffee.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+const FilterCoffee = () => {
+  const [filterValue, setFilterValue] = useState("");
+  const { push } = useRouter();
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams();
+    if (filterValue) {
+      searchParams.set("order", filterValue);
+      push(`?${searchParams.toString()}`);
+    }
+  }, [filterValue]);
+
+  return (
+    <div>
+      <div>
+        <select
+          className="border px-10 outline-none"
+          onChange={(e) => setFilterValue(e.target.value)}
+        >
+          <option value="expensive">گران ترین</option>
+          <option value="cheap">ارزان ترین</option>
+          <option value="mix">میکس</option>
+          <option value="pure">خالص</option>
+          <option value="newset">جدیدترین</option>
+          <option value="oldest">قدیمی ترین</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default FilterCoffee;
