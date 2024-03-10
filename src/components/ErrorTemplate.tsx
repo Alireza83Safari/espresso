2c11f4d730315794b294b2ec3aa073a124ece356
Alireza <alireza83safarii@gmail.com>
2024-03-10 18:57:49 +0330

    style: change button color

--- src/components/ErrorTemplate.tsx
+++ src/components/ErrorTemplate.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-export default function ErrorTemplate({reset }: any) {
+export default function ErrorTemplate({ reset }: any) {
   return (
-    <div className="flex flex-col items-center justify-center h-screen font-sans">
-      <h2 className="text-2xl font-bold mb-6">اوه! مشکلی پیش آمد!</h2>
+    <div className="flex h-screen flex-col items-center justify-center font-sans">
+      <h2 className="mb-6 text-2xl font-bold">اوه! مشکلی پیش آمد!</h2>
       <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        className="focus:shadow-outline rounded bg-green px-4 py-2 font-bold text-white hover:bg-[#0A5B01] focus:outline-none"
         onClick={() => reset()}
       >
         دوباره امتحان کنید
